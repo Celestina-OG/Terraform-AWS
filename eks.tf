3478847fd1418cbc8a654539a924c18ba4213e54
Celestina <amadi@blackcopper.io>
2023-08-05 02:28:33 +0100

    modified eks.tf and added app.yaml file

--- eks.tf
+++ eks.tf
@@ -48,7 +48,7 @@ resource "aws_eks_cluster" "eks" {
   role_arn = aws_iam_role.eks_cluster.arn
 
   # (Optional) Desired kubernetes master version
-  version = "1.26"
+  version = "1.27"
 
   vpc_config {
     # Indicates whether or not EKS private API server endpoint is enabled
