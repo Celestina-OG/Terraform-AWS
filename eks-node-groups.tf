95947fbc5b068cd7af18635c1749575ad22db661
Celestina <amadi@blackcopper.io>
2023-08-06 10:22:44 +0100

    upgraded kubernetes version

--- eks-node-groups.tf
+++ eks-node-groups.tf
@@ -108,7 +108,7 @@ resource "aws_eks_node_group" "nodes_general" {
   }
 
   # Kubernetes version (optional)
-  version = "1.26"
+  version = "1.27"
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
